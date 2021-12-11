3c1c811cead1abae46dd9690a9815674ccde11d1
Makar <makar.melikyan@gmail.com>
2021-12-12 01:28:02 +0400

    state managed file upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        <code>src/App.tsx</code>
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const App = () => {
+  const [file, setFile] = useState<File | undefined>();
+  const changeHandler = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFile(event?.target?.files?.[0]);
+  };
+
+  useEffect(() => {
+    console.info(file);
+  }, [file]);
+
+  return (
+    <div className="App">
+      <input type="file" name="file" onChange={changeHandler} />
+    </div>
+  );
+};
 
 export default App;
